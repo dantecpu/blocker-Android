458203f82c7df13b1a04dc6ea49103a1535db2dc
NewBugger <contacts@sometimesnaive.org>
2020-06-12 15:37:21 +0800

    [correct] shizuku: 4.0.0 upgrade vol-1.0.2

--- app/src/main/java/io/github/newbugger/android/blocker/shizuku/ShizukuBinder.kt
+++ app/src/main/java/io/github/newbugger/android/blocker/shizuku/ShizukuBinder.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.util.Log
 import androidx.core.app.ActivityCompat.checkSelfPermission
 import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import moe.shizuku.api.ShizukuApiConstants
 
 
@@ -16,14 +17,15 @@ object ShizukuBinder {
     private const val REQUEST_CODE_PERMISSION_V3 = 1
 
     fun shizukuRequestPermission(context: Context): Boolean {
-        return if (checkSelfPermission(context, ShizukuApiConstants.PERMISSION) !=
-                PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(context as Activity,
-                    arrayOf(ShizukuApiConstants.PERMISSION), REQUEST_CODE_PERMISSION_V3)
-            Log.d(TAG, "requesting shizuku permission ..")
+        if (checkSelfPermission(context, ShizukuApiConstants.PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(context as Activity, arrayOf(ShizukuApiConstants.PERMISSION), REQUEST_CODE_PERMISSION_V3)
+            Log.d(TAG, "requesting shizuku permission")
+        }
+        return if (shouldShowRequestPermissionRationale(context as Activity, ShizukuApiConstants.PERMISSION)) {
+            Log.d(TAG, "user denied the shizuku permission (shouldShowRequestPermissionRationale=true)")
             false
         } else {
-            Log.d(TAG, "shizuku permission requested.")
+            Log.d(TAG, "requested shizuku permission")
             true
         }
     }
