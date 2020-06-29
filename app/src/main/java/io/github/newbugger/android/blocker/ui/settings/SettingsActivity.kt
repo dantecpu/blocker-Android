0690aa9022657ebe7f16f50e7d0bea954cff4315
NewBugger <contacts@sometimesnaive.org>
2020-06-29 18:13:38 +0800

    [correct] move: ui

--- app/src/main/java/io/github/newbugger/android/blocker/ui/settings/SettingsActivity.kt
+++ app/src/main/java/io/github/newbugger/android/blocker/ui/settings/SettingsActivity.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import io.github.newbugger.android.blocker.R
-import io.github.newbugger.android.blocker.util.setupActionBar
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        setupActionBar(R.id.toolbar) {
-            setDisplayHomeAsUpEnabled(true)
-        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
